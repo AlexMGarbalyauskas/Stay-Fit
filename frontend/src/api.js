02e648ba12a421ae1ede40d84dc5022d25d97e7e
Alex <alexgarbalyauskas@gmail.com>
2025-12-06 18:57:39 +0000

    new

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:4000/api'; // Replace with Render URL when deployed
+const API_BASE = 'https://stay-fit-1.onrender.com/api';
 
 export const register = (username, email, password) => 
   axios.post(`${API_BASE}/register`, { username, email, password });
@@ -9,4 +9,6 @@ export const login = (email, password) =>
   axios.post(`${API_BASE}/login`, { email, password });
 
 export const getMe = (token) =>
-  axios.get(`${API_BASE}/me`, { headers: { Authorization: `Bearer ${token}` } });
+  axios.get(`${API_BASE}/me`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
