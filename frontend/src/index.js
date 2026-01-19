02d26a094dbe24db1b7a9386baec3913835293f4
Alex <alexgarbalyauskas@gmail.com>
2026-01-19 14:35:32 +0000

    re-enable service worker for PWA install

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -43,13 +43,11 @@ root.render(
   </React.StrictMode>
 );
 
-// Disable service worker temporarily to fix loading issues
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => console.log('SW registered:', registration.scope))
-//       .catch(err => console.log('SW registration failed:', err));
-//   });
-// }
+// Register service worker for PWA installability
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => console.log('SW registered:', registration.scope))
+    .catch(err => console.log('SW registration failed:', err));
+}
 
 reportWebVitals();
